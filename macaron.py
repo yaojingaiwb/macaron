3f8483ea3c7be27df4c333033c32b1b780f56ba2
yaojingaiwb <81913510+yaojingaiwb@users.noreply.github.com>
2025-10-04 13:38:01 +0800

    Update macaron.py

--- macaron.py
+++ macaron.py
@@ -280,8 +280,9 @@ async def main(filePath, proxyPath, tread):
 
 if __name__ == "__main__":
     _filePath = r"自己的目标目录 或者 运行目录"
-    _proxyPath = r"自己的目标目录"
+    _proxyPath = r"自己的目标目录 或者 运行目录"
     _tread = 1
     asyncio.run(main(_filePath, _proxyPath, _tread))
+
 
 
